5fbf7faef621ea965e2f6ba39a39e4b8c0ffea4a
Bae Junbeom <beajun4958@gmail.com>
2022-07-16 11:03:45 +0900

    commit

--- SCPC/2번 K등분.cpp
+++ SCPC/2번 K등분.cpp
@@ -1,27 +1,40 @@
 #include <iostream>
 #include <string>
-
+#include <set>
+#include <map>
+#include <utility>
 using namespace std;
 int plist[500000] = {};
 
-int fpsum(int n, int start, int idx, int cut){
+
+int fpsum(int n, int start, int idx, int divc, int div){
     // n: 목표값, start 시작 인덱스, idx 수열 원소 개수
+    map <pair<int, int>, int> mem;
     int cnt = 0;
     int sum = 0;
     int end = start;
-    while(start <= idx){
-        while(sum < n && end < idx){
-            sum += plist[end];
-            end++;
-        }
-        if(sum == n){
-            return fpsum(n, end, idx, cut)+1;
-        } 
-        else{ // 부분합 더 클 경우
-            return 0;
-        }
-        //cout << sum << endl;
+    if(divc < div) return 0;
+    if(end >= idx){
+        if(divc == div) return 1;
+        else return 0;
     }
+    while(end < idx){
+        sum += plist[end];
+        end++;
+        if(sum == n){
+            if(mem.count(make_pair(end, div+1))){
+                cnt+= mem.find(make_pair(end, div+1))->second;
+            }
+            else{
+                int tcnt = fpsum(n, end, idx, divc, div+1);
+                mem.insert(make_pair(make_pair(end, div+1), tcnt));
+                cnt += tcnt;
+            }
+        } 
+    }
+    return cnt;
+        //cout << sum << endl;
+
     
 }
 
@@ -37,7 +50,7 @@ void sol(int t)
         sum += plist[i];
     }
     cout << "Case #" << t+1 << endl; 
-    cout << fpsum(sum/k, 0, idx, k) << endl;
+    cout << fpsum(sum/k, 0, idx, k, 0) << endl;
     
 }
 
@@ -45,7 +58,7 @@ int main()
 {
     std::ios_base::sync_with_stdio(false);
     std::cout.tie(NULL);
-
+    std::cin.tie(NULL);
     int t;
     cin >> t;
     for(int i = 0 ; i < t; i++){
